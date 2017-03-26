ad5630fe9582a931bba217f5ef99a316ae22e17d
Ash Wilson <smashwilson@gmail.com>
2017-03-26 19:49:27 -0400

    Line endings in Docker scripts

--- script/services-up.ps1
+++ script/services-up.ps1
@@ -1,7 +1,8 @@
-docker run -d -p 6379:6379 -e POSTGRES_USER=markov -e POSTGRES_PASSWORD=shhh --name redis redis
-docker run -d -p 5432:5432 --name postgres postgres:9.6
+docker run -d -p 6379:6379 --name redis redis
+docker run -d -p 5432:5432 -u postgres -e POSTGRES_USER=markov -e POSTGRES_PASSWORD=shhh --name postgres postgres:9.6
 
 $env:REDIS_URL = "redis://localhost:6379"
 $env:DATABASE_URL = "postgres://markov:shhh@localhost/markov"
+$env:DATABASE_SSL = "false"
 
 Write-Output "Services up"
